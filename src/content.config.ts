7e8814968e69fd8ba3c5d66e2aafae935dd60d36
IrohaMo <irohamo@irohamo.dev>
2026-04-28 12:49:05 +0900

    add components

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from 'astro/loaders';
 export const collections = {
 	work: defineCollection({
 		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/work', pattern: '**/*.md' }),
+        loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
@@ -14,13 +14,20 @@ export const collections = {
 			img_alt: z.string().optional(),
 		}),
 	}),
-	article: defineCollection({
-		loader: glob({ base: './src/content/article', pattern: '**/*.md' }),
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()).default([]),
-		}),
-	}),
+    article: defineCollection({
+        loader: glob({ base: './src/content/article', pattern: '**/*.{md,mdx}' }),
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            publishDate: z.coerce.date(),
+            tags: z.array(z.string()).default([]),
+        }),
+    }),
+    mention: defineCollection({
+        loader: glob({ base: './src/content/mention', pattern: '**/*.{md,mdx}' }),
+        schema: z.object({
+            title: z.string().optional(),
+            publishDate: z.coerce.date().optional(),
+        }),
+    }),
 };
